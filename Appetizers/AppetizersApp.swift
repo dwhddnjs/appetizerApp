7ab9b4b199fd005559edc401034340d1614cd9cb
dwhddnjs <per1215@naver.com>
2024-06-10 02:11:11 +0900

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by 이종원 on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
